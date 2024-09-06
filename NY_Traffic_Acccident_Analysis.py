19ad4725ea7ded57b2b2244d613629104c2ba2d2
Mike <88722598+mike-ua@users.noreply.github.com>
2024-09-06 21:15:11 +0300

    Update NY_Traffic_Acccident_Analysis.py
    changed line 103 
    from: = df_filtered['CONTRIBUTING_FACTOR_VEHICLE_1'].replace(category_map)
    to: = df_filtered.replace(category_map)

--- NY_Traffic_Acccident_Analysis.py
+++ NY_Traffic_Acccident_Analysis.py
@@ -100,7 +100,7 @@ category_map = {
 df_filtered = df.isin(category_map.keys())
 
 # Replace the contributing factor categories with the new groups
-df_filtered['Grouped Factor'] = df_filtered['CONTRIBUTING_FACTOR_VEHICLE_1'].replace(category_map)
+df_filtered['Grouped Factor'] = df_filtered.replace(category_map)
 
 # Count the occurrences of each grouped factor
 grouped_factor_counts = df_filtered['Grouped Factor'].value_counts()
